1b0d14ca4b9a025cee3e6b578a363f338f3ceefe
amazing-shubham <smartshubhamgupta123@gmail.com>
2024-03-16 22:39:20 +0530

    Episode 1 - Inception's Code

---
+++ App.js
@@ -0,0 +1,47 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement(
+            "div",
+            { id: "child1" },
+            [
+                React.createElement(
+                    "h1",
+                    {},
+                    "I am H1 Tag"
+                ),
+                React.createElement(
+                    "h2",
+                    {},
+                    "I am H2 Tag"
+                )
+            ]
+        ),
+        React.createElement(
+            "div",
+            { id: "child2" },
+            [
+                React.createElement(
+                    "h1",
+                    {},
+                    "I am H1 Tag"
+                ),
+                React.createElement(
+                    "h2",
+                    {},
+                    "I am H2 Tag"
+                )
+            ]
+        )
+    ]
+);
+
+const heading = React.createElement
+    (
+        "h1",
+        { id: "heading" },
+        "Hello World from React!"
+    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
