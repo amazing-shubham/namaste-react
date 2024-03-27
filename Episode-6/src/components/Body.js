e73e128e079780bae8afccd29732e7013544095a
amazing-shubham <smartshubhamgupta123@gmail.com>
2024-03-27 23:51:46 +0530

    Episode-6 : Exploring the world Part 1

---
+++ Episode-6/src/components/Body.js
@@ -0,0 +1,48 @@
+import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
+
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurant(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (x) => x.info.avgRating > 4.3
+            );
+            setListOfRestaurant(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resList={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
