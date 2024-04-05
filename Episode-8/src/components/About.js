cf499c9740908f46d14f9bfe89d58f928256cf43
amazing-shubham <smartshubhamgupta123@gmail.com>
2024-04-05 22:50:44 +0530

    Episode:8 - Let's Get classy part3

--- Episode-8/src/components/About.js
+++ Episode-8/src/components/About.js
@@ -1,13 +1,26 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div className="about">
-      <h1>About Page</h1>
-      <User />
-      <UserClass name="Shubham Gupta" location="Dhanbad" />
-    </div>
-  );
-};
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Mounted");
+  }
+  render() {
+    console.log("Parent rendered");
+    return (
+      <div className="about">
+        <h1>About Page</h1>
+        <User />
+        <UserClass name="Shubham Gupta" location="Dhanbad" />
+      </div>
+    );
+  }
+}
 
 export default About;
--- Episode-8/src/components/UserClass.js
+++ Episode-8/src/components/UserClass.js
@@ -2,12 +2,30 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    //console.log(props);
+    this.state = {
+      count: 0,
+    };
+    console.log(this.props.name + "Child Constructor");
+  }
+  componentDidMount() {
+    console.log(this.props.name + "Child Mounted");
   }
   render() {
+    console.log(this.props.name + "Child rendered");
     const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
+        <h2>Count : {count}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Counter
+        </button>
         <h2>Name : {name}</h2>
         <h2>Location : {location}</h2>
         <h3>Contact : @crazy_shubhi</h3>
