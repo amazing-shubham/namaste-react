c5f3400636563d1824b795c1482e7432be61975b
amazing-shubham <smartshubhamgupta123@gmail.com>
2024-04-06 15:04:14 +0530

    Episode:9 Folder skeleton

---
+++ Episode-9/src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name : Shubham</h2>
+      <h2>Location : Dhanbad</h2>
+      <h3>Contact : @crazy_shubhi</h3>
+    </div>
+  );
+};
+
+export default User;
